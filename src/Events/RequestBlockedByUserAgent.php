7495a5ee63ff7e095bc5388fc2556419e34be2cf
xqus <203013+xqus@users.noreply.github.com>
2025-11-30 20:19:51 +0000

    Fix styling

--- src/Events/RequestBlockedByUserAgent.php
+++ src/Events/RequestBlockedByUserAgent.php
@@ -5,11 +5,11 @@ namespace xqus\BadBot\Events;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
- 
+
 class RequestBlockedByUserAgent
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
- 
+
     /**
      * Create a new event instance.
      */
