faed7565b0b123b235f47a1004ffd22e8d8d1da0
Audun Larsen <larsen@xqus.com>
2025-11-30 14:26:32 +0100

    🔥Remove unused route

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,1 @@
 <?php
-
-use Illuminate\Support\Facades\Route;
-
-Route::get('robots.txt', function () {
-    return response()
-        ->view('bad-bot::robots')
-        ->header('Content-Type', 'text/plain');
-});
