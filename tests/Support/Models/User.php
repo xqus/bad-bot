83d36e59afe1c448838f048ac2ad1b2aa9094024
Audun Larsen <larsen@xqus.com>
2025-12-01 18:33:55 +0100

    ✅Fix test

--- tests/Support/Models/User.php
+++ tests/Support/Models/User.php
@@ -5,9 +5,8 @@ declare(strict_types=1);
 namespace Tests\Support\Models;
 
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Tests\Support\Database\Factories\UserFactory;
+use Tests\Support\database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
 
 class User extends Authenticatable
 {
